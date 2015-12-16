67f889442e1da6a72203bc0f77584167efdfda5b
kngo0 <kevin.ngo707@hotmail.com>
2015-12-16 13:01:50 +0100

    MyMovies - Commit 8

--- app/routes.php
+++ app/routes.php
@@ -10,13 +10,13 @@ $app->get('/', function () use ($app) {
 // Détails sur un film
 $app->get('/movie/{id}', function($id) use ($app) {
     $movie = $app['dao.movie']->find($id);
-    return $app['twig']->render('film.html.twig', array('movie' => $film));
+    return $app['twig']->render('film.html.twig', array('movie' => $movie));
 })->bind('movie');
 
 // Liste de tous les films
 $app->get('/movie/', function() use ($app) {
     $movies = $app['dao.movie']->findAll();
-    return $app['twig']->render('films.html.twig', array('movies' => $films));
+    return $app['twig']->render('films.html.twig', array('movies' => $movies));
 })->bind('movies');
 
 
